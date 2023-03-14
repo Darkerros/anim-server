9c47b67ec36525f75736f33ba760d90d3e2682ef
darker <darkerros@yandex.ru>
2023-03-14 23:22:56 +0300

    feat: add user craetion attributes in user model

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,8 +1,15 @@
 import { Column, Model, Table, DataType, HasMany } from 'sequelize-typescript';
 import { UserSessionsModel } from './user-sessions.model';
 
+interface UserCreationsAttributes {
+  nickname: string;
+  email: string;
+  password: string;
+  avatarUrl?: string;
+}
+
 @Table({ tableName: 'Users' })
-export class UserModel extends Model<UserModel> {
+export class UserModel extends Model<UserModel, UserCreationsAttributes> {
   @Column({
     type: DataType.INTEGER,
     primaryKey: true,
@@ -19,6 +26,9 @@ export class UserModel extends Model<UserModel> {
 
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
+
+  @Column({ type: DataType.STRING, allowNull: true })
+  avatarUrl: string;
 
   @HasMany(() => UserSessionsModel)
   sessions: UserSessionsModel[];
