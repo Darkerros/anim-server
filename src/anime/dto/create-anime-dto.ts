ec63c53d3a5072175f77b8e005a08c60317807a4
darker <darkerros@yandex.ru>
2023-03-16 22:36:31 +0300

    feat: create "create anime dto"

---
+++ src/anime/dto/create-anime-dto.ts
@@ -0,0 +1,54 @@
+export interface CreateAnimeDto {
+  kodikId: string;
+
+  shikimoryId: string;
+
+  shikimoriRating: number;
+
+  shikimoriVotes: number;
+
+  kinopoiskId: string;
+
+  kinopoiskRating: number;
+
+  kinopoiskVotes: number;
+
+  imdbId: string;
+
+  imdbRating: number;
+
+  imdbVotes: number;
+
+  title: string;
+
+  titleOrig: string;
+
+  otherTitle: string;
+
+  otherTitleEnList: string[];
+
+  year: number;
+
+  episodesCount: number;
+
+  lastEpisode: number;
+
+  kind: string; //"tv"
+
+  status: string; //"released"
+
+  description: string;
+
+  posterUrl: string;
+
+  screenshots: string[];
+
+  duration: number;
+
+  // genres: string[];
+  episodesTotal: number;
+
+  episodesAired: number;
+
+  kodikDataId: number;
+}
